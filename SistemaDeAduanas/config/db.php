f77017e61ff5bd711f4e967e0d7f3662d8c5f0bc
kixxxi <kr.acevedo@duocuc.cl>
2026-06-15 15:10:49 -0400

    Datos de conexion base de datos

--- SistemaDeAduanas/config/db.php
+++ SistemaDeAduanas/config/db.php
@@ -1,11 +1,8 @@
 <?php
-//config/db.php
-
-/* Datos de conexion a la base de datos */
-
+// config/db.php
+// Datos de conexión a la base de datos
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');         
-define('DB_NAME', 'aduanas_chile'); 
-
+define('DB_USER', 'root'); // Cambia esto si tu usuario es distinto
+define('DB_PASS', '');     // Pon tu contraseña si tienes una
+define('DB_NAME', 'sistema_aduanas'); // Cambia por el nombre de tu BD
 ?>
