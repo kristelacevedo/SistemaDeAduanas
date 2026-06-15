eca3a9e978546768553ea6c229c48bf91da0107b
kixxxi <kr.acevedo@duocuc.cl>
2026-06-15 15:15:34 -0400

    navbar

---
+++ SistemaDeAduanas/navbar.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php"><i class="fas fa-ship"></i> Aduanas Web</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="personas.php">Personas</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="movimientos.php">Movimientos</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link text-danger" href="login.php?logout=true"><i class="fas fa-sign-out-alt"></i> Salir</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
